c9963f9ca16d39c762e942a591a34f5a82fc98aa
Polina Kulik <pollyandrrrr@yandex.ru>
2023-10-25 21:57:57 +0300

    change anima tech, info, add wowjs

--- src/Info/Info.js
+++ src/Info/Info.js
@@ -2,10 +2,29 @@ import './Info.css';
 
 import polyana from '../images/polyana.jpg';
 
+import {useEffect} from 'react';
+
+import {WOW} from 'wowjs';
+
+
 function Info() {
+    useEffect(() => {
+        // Создайте экземпляр WOW.js
+        const wow = new WOW(
+                      {
+                      boxClass:     'wow',      // default
+                      animateClass: 'animated', // default
+                      offset:       200,          // default
+                      mobile:       true,       // default
+                      live:         true        // default
+                    }
+                    )
+                    wow.init();
+    }, []);
+
     return (
         <section className='info'>
-            <p className='info__text animate__animated animate__fadeInUp animate__slow 3s'><span className='info__text-color'>Hi there!</span> My name is <span className='info__text-color'>Lina</span>, <br /> I am a <span className='info__text-color'>beginner 
+            <p className='info__text wow slideInLeft slower'><span className='info__text-color'>Hi there!</span> My name is <span className='info__text-color'>Lina</span>, <br /> I am a <span className='info__text-color'>beginner 
             web-developer</span> and here you can find <span className='info__text-color'>my portfolio</span> c: </p>
             <div className='info__frame'>
                 <img className='info__img' src={polyana} alt='фото автора'/>
--- src/Stack/Stack.js
+++ src/Stack/Stack.js
@@ -7,28 +7,31 @@ import react from '../images/react.jpg';
 import js from '../images/js.jpg';
 import mongodb from '../images/mongodb.png';
 
-import { motion } from "framer-motion";
-import { useInView } from "react-intersection-observer";
+import {useEffect} from 'react';
 
+import {WOW} from 'wowjs';
 
 
 export default function Stack() {
-    const [ref, inView] = useInView({
-        triggerOnce: true,
-        threshold: 0.7,
-      });
+    useEffect(() => {
+        // Создайте экземпляр WOW.js
+        const wow = new WOW(
+            {
+                boxClass:     'wow',
+                animateClass: 'animated',
+                offset:       300,
+                mobile:       true,
+                live:         true,
+            }
+            )
+            wow.init();
+    }, []);
+
 
     return (
         <section className='stack'>
-            <motion.div 
-                 ref={ref}
-                 initial={{ opacity: 0, y: 0 }}
-                 animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 70 }}
-                 transition={{ duration: 3 }}
-            >
-            <h1 className='stack__title animate__animated animate__backInLeft animate__slow 3s'>My Tech Stack</h1>
-            <p className='stack__subtitle animate__animated animate__backInLeft animate__slow 3s'>Technologies I've been working with recently</p>
-            </motion.div>
+            <h1 className='stack__title wow rotateInDownLeft'>My Tech Stack</h1>
+            <p className='stack__subtitle wow rotateInDownLeft'>Technologies I've been working with recently</p>
             <div className='stack__components'>
                 <img className='stack__tech' alt='css' src={css} />
                 <img className='stack__tech' alt='java script' src={js}/>
